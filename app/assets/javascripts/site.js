79ba997cc203f8357d0eb381d99fd85e9f6bc86e
yunwei <stillwyw@gmail.com>
2017-02-23 02:06:58 +0800

    enabled getting location from user browser, added more style, set default place to tokyo.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -66,16 +66,40 @@ var ready = function() {
     methods: {
       initializeMap: function() {
         // picked shanghai as the point.
-        var shanghai = new google.maps.LatLng(31.2304, 121.4737);
+        var userLocation = new google.maps.LatLng(35.6895, 139.6917);
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function (position) {
+            userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          }, function (error) {
+          	switch(error.code) {
+          		case error.PERMISSION_DENIED:
+          			console.log("User denied the request for Geolocation.");
+          			break;
+          		case error.POSITION_UNAVAILABLE:
+          			console.log("Location information is unavailable.");
+          			break;
+          		case error.TIMEOUT:
+          			console.log("The request to get user location timed out.");
+          			break;
+          		case error.UNKNOWN_ERROR:
+          			console.log("An unknown error occurred.");
+          			break;
+          	}
+          }, {
+        		enableHighAccuracy : true,
+        		timeout : Infinity,
+        		maximumAge : 0
+        	});
+        }
 
         map = new google.maps.Map(document.getElementById('map'), {
-          center: shanghai,
-          zoom: 15
+          center: userLocation,
+          zoom: 13
         });
 
         var request = {
-          location: shanghai,
-          radius: '500',
+          location: userLocation,
+          radius: '1000',
           query: 'hotel'
         };
 
@@ -84,8 +108,19 @@ var ready = function() {
       },
       hotelSearchCallBack: function(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
           this.hotels = results;
         }
+      },
+      createMarker: function(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location,
+          placeId: place.id
+        });
       }
     }
   })
@@ -221,7 +256,7 @@ var ready = function() {
 
   var HotelItem = Vue.component('hotel-item', {
     props: ['hotel'],
-    template: '<div class="hotel-item" v-on:click="clicked">\
+    template: '<div class="hotel-item" name={{hotel.id}} v-on:click="clicked">\
                   <div class="img-wrapper">\
                     <img v-bind:src="this.photo()" class="img-responsive"/>\
                   </div>\
