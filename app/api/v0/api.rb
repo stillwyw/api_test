dfa5be297dd047348e22ab6317d223c1c9e76bb6
yunwei <stillwyw@gmail.com>
2017-02-23 13:59:52 +0800

    fulfilled authorisations to api requests.

--- app/api/v0/api.rb
+++ app/api/v0/api.rb
@@ -10,7 +10,6 @@ module API
       helpers Doorkeeper::Grape::Helpers
       
       helpers do
-        
         def precheckin_request_params
           ActionController::Parameters.new(params).require(:precheckin_request).permit(:hotel, :city, :arrival_date, :checkout_date)
         end
@@ -24,9 +23,9 @@ module API
         end
 
         def current_user
-          User.find_by_id current_token.resource_owner_id
+          @current_user ||= (User.find_by_id(current_token.resource_owner_id) if current_token)
         end
-
+        
         def current_scopes
           current_token.scopes
         end
@@ -38,11 +37,13 @@ module API
           if current_user
             current_user.precheckin_requests
           else
+            # no resource owner, then this became a public api for pulling all precheckin requests
+            # as the text file indicated.
             PrecheckinRequest.all
           end
         end
         
-        desc "View a single precheckin request."
+        desc "View a single precheckin request. Currently public to all."
         params do
           requires :id, type: Integer, desc: 'PrecheckinRequest id.'
         end
@@ -55,7 +56,7 @@ module API
       end
       
       resource :user do
-        desc "Get current resource owner profile"
+        desc "Get current resource owner profile."
         get do
           authorize!
           
@@ -67,13 +68,15 @@ module API
         
         segment '/:user_id' do
           resource :precheckin_requests do
-            desc "Get a user's all precheckin_requests"
+            desc "Get a user's all precheckin_requests. get: /api/v0/:user_id/precheckin_requests "
             get do
+              authorize!
               User.find(params[:user_id]).precheckin_requests.includes(:user)
             end
             
-            desc "Create a precheckin_request for user"
+            desc "Create a precheckin_request for user post: /api/v0/:user_id/precheckin_requests "
             post do
+              authorize!
               pr = User.find(params[:user_id]).precheckin_requests.build(precheckin_request_params)
               if pr.save!
                 pr
